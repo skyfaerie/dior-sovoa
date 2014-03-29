38ec4a0c1e277d7e6acc5f1283c1575a01d466c8
Aisha Green <skyfaerie22@gmail.com>
2014-03-29 11:15:43 -0400

    home page using main nav styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,6 @@
 /* RESPONSIVE NAV
 /**************************************/
 
-/*$("nav").addClass("js").before(
-	'<div id="menu" style="float:right;">&#9776;</div>'
-	);*/
-
-  $("#menu").click(function(){
-    $("nav").toggle();
-  });
-
- /* $(window).resize(function(){
-    if(window.innerWidth > 768) {
-      $("nav").removeAttr("style");
-    }*/
-  // });
+$("#menu").click(function(){
+  $("nav").toggle();
+});
