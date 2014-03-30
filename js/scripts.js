b711442222d8f55b63a548be0a1ad197b8e827cf
Aisha Green <skyfaerie22@gmail.com>
2014-03-30 19:09:32 -0400

    added masonsy to client list

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@
 $("#menu").click(function(){
   $("nav").toggle();
 });
+
+/**************************************/
+/* MASONRY JS
+/**************************************/
+
+var $container = $('#container');
+// initialize
+$container.masonry({
+  columnWidth: 200,
+  itemSelector: '.item'
+});
